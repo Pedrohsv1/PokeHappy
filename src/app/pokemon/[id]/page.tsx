f1e0f13d8d40702703a312f1096bf22eb9c6db92
Pedro Varela <pedrao.varela@gmail.com>
2024-07-14 20:51:42 -0300

    feat: card stat component

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -33,9 +33,9 @@ import { useToast } from "@/components/ui/use-toast";
 import { MoveCard } from "@/components/move-card";
 import { EvolutionSolo } from "@/components/evolutions";
 import { BreadCrumbComp, BreadProps } from "@/components/breadcrum";
+import { CardStat } from "@/components/card-stat";
 
 export default function PokemonPage() {
-  const { toast } = useToast();
   const breads: BreadProps = {
     breads: [
       {
@@ -45,7 +45,10 @@ export default function PokemonPage() {
     ],
     namePage: "Solo Pokemon",
   };
+
+  const { toast } = useToast();
   const router = useParams();
+
   const [pokemon, setPokemon] = useState<Pokemon>();
   const mutatePokemon = useMutation(GetPokemon, {
     onSettled: (data) => {
@@ -132,66 +135,7 @@ export default function PokemonPage() {
 
           <div className="h-full flex flex-col gap-4">
             {pokemon ? (
-              <Card>
-                <CardHeader className="flex flex-col">
-                  <TypographyH2>Stats</TypographyH2>
-                  <CardDescription>Default stats</CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                  <div className="grid grid-cols-3 gap-4">
-                    {pokemon.stats.map((stat) => (
-                      <div className="flex flex-col gap-2">
-                        <div className="flex gap-2">
-                          {stat.stat.name === "hp" ? (
-                            <HeartCrack className="size-6" />
-                          ) : stat.stat.name === "attack" ? (
-                            <Sword className="size-6" />
-                          ) : stat.stat.name === "defense" ? (
-                            <Shield className="size-6" />
-                          ) : stat.stat.name === "special-attack" ? (
-                            <Swords className="size-6" />
-                          ) : stat.stat.name === "special-defense" ? (
-                            <ShieldHalf className="size-6" />
-                          ) : (
-                            <Wind className="size-6" />
-                          )}
-                          <p>
-                            {stat.stat.name === "hp"
-                              ? "Health Point"
-                              : stat.stat.name === "attack"
-                              ? "Attack"
-                              : stat.stat.name === "defense"
-                              ? "Defense"
-                              : stat.stat.name === "special-attack"
-                              ? "Special Attack"
-                              : stat.stat.name === "special-defense"
-                              ? "Special Defense"
-                              : "Speed"}
-                          </p>
-                        </div>
-
-                        <p
-                          className={`text-md  ${
-                            stat.stat.name === "hp"
-                              ? "text-emerald-400"
-                              : stat.stat.name === "attack"
-                              ? "text-red-300"
-                              : stat.stat.name === "defense"
-                              ? "text-blue-300"
-                              : stat.stat.name === "special-attack"
-                              ? "text-red-400"
-                              : stat.stat.name === "special-defense"
-                              ? "text-blue-400"
-                              : "text-yellow-400"
-                          }`}
-                        >
-                          {stat.base_stat}
-                        </p>
-                      </div>
-                    ))}
-                  </div>
-                </CardContent>
-              </Card>
+              <CardStat stats={pokemon.stats} />
             ) : (
               <Card className="flex flex-col gap-2 p-4">
                 <Skeleton className="w-20 h-8 mt-2" />
